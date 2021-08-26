ca63450b6ba2d27fa554111a567378dfc156f099
MrWalshyType2 <67016030+MrWalshyType2@users.noreply.github.com>
2021-08-26 12:38:26 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -14,8 +14,9 @@ echo "PORT 80: " + $(sudo lsof -t -i:80)
 #  sudo kill -9 $PID;
 #fi;
 
-cd actions-test-spring-app;
-mvn package;
+cd actions-test-spring-app
+mvn package
 
 # no hangup (nohup) prevents app from stopping after logout, & puts in background;
-sudo nohup java -jar ./target/spring_cucumber-0.0.1-SNAPSHOT.jar -> /dev/null
+# fix for race condition: https://stackoverflow.com/questions/29142/getting-ssh-to-execute-a-command-in-the-background-on-target-machine
+sudo nohup java -jar ./target/spring_cucumber-0.0.1-SNAPSHOT.jar > foo.out 2> foo.err < /dev/null &
